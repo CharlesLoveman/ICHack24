a1223c446534535437ad200e39a8b11a9fdf39fe
Niall Oswald <niall.oswald@hotmail.co.uk>
2024-02-03 22:34:03 +0000

    Add tests for create_pokemon

--- Test/test_gemini.py
+++ Test/test_gemini.py
@@ -7,5 +7,15 @@ import pytest
 
 @pytest.mark.parametrize("name", ("banana", "bicycle", "flowers"))
 def test_get_gemini_response(name):
-    img = load_image_from_file(f"TestImage/{name}.jpg")
+    img = load_image_from_file(f"TestImages/{name}.jpg")
     response = get_gemini_response(GEMINI_PROMPT_TEMPLATE, img)
+
+
+@pytest.mark.parametrize("name", ("banana", "bicycle", "flowers"))
+def test_create_pokemon(name):
+    img = load_image_from_file(f"TestImages/{name}.jpg")
+    name, pokedex, stats = create_pokemon(GEMINI_PROMPT_TEMPLATE, img)
+
+    assert isinstance(name, str)
+    assert isinstance(pokedex, str)
+    assert isinstance(stats, dict)
