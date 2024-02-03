05734fffac552b9b20563a877d1e865b407569c3
Niall Oswald <niall.oswald@hotmail.co.uk>
2024-02-03 21:49:59 +0000

    Initial implementation of Gemini api

---
+++ Backend/prompt_templates.py
@@ -0,0 +1,26 @@
+"""Prompt templates for Gemini."""
+
+GEMINI_PROMPT_TEMPLATE = """Your task is to create new Pokemon based on images users take. The Pokemon you create should be entertaining and take into account the attributes of the object which may influence its stats. You are required to produce a brief Pokedex style entry which describes the Pokemon as well as numerical stats for the Pokemon. Numerical values for stats must be between 0 and 255 inclusive.
+
+[Output Pokedex]
+
+[End Output Pokedex]
+
+[Output Stats]
+
+Type:
+
+HP:
+
+Attack:
+
+Defence:
+
+Special Attack:
+
+Special Defence:
+
+Speed:
+
+[End Output Stats]
+"""
