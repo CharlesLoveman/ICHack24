b24f0344f43b3dec9495dbc624e85dcf0fca2cdb
Charles Loveman <cel20@ic.ac.uk>
2024-02-03 17:40:59 +0000

    fix: CORS credentials

--- Backend/app.py
+++ Backend/app.py
@@ -3,13 +3,12 @@ from flask_cors import CORS
 from flask_socketio import SocketIO, send, emit
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/app": {"origins": "*"}})
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_credentials=True, cors_allowed_origins="*")
 
 if __name__ == '__main__':
     socketio.run(app)
-
 
 @socketio.on('message')
 def handle_message(message):
@@ -21,9 +20,9 @@ def handle_json(json):
 
 @socketio.on('foo')
 def handle_my_custom_event(json):
-    #emit('my response', json)
-    print(json)
-    send('foo')
+    emit('foo', json)
+    #print(json)
+    #send('foo')
 
 
 
