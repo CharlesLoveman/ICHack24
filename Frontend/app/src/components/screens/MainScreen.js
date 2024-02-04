9a18f8cf6a5f6875fab3f081636a2f3368b910d9
Akesh <akeshmistry15@gmail.com>
2024-02-04 09:11:54 +0000

    Small Modifications

--- Frontend/app/src/components/screens/MainScreen.js
+++ Frontend/app/src/components/screens/MainScreen.js
@@ -5,20 +5,31 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import JoinRoomInputBox from '../JoinRoomInputBox.js'
 import { BrowserRouter as Router, Link } from "react-router-dom";
+import axios from 'axios';
 
 import { socket } from '../../socket';
 
+ const player_id = str(randrange(0, 1000000))
+
 export default function MainScreen() {
 
-  function createBattle(pokemon) {
-
-    if (pokemon) {
-      socket.emit('createBattle', { player_id: document.cookie["player_id"], pokemon_id: pokemon.id })
+  function fetchPokemon()
+  {
+    pokemons = axios.get(`http://127.0.0.1:5000/ListPokemon/${player_id}`);
+    if (pokemons)
+    {
+      pokemon = pokemons[0];
     }
-
   }
 
-  const [pokemon, setPokemon] = useState();
+  fetchPokemon()
+
+  function createBattle(pokemon) {
+    if (pokemon) {
+      socket.emit('createBattle', { player_id: document.cookie["player_id"], pokemon_id: pokemon.id });
+    }
+  }
+      
   function createBattle(pokemon) {
 
     if (pokemon) {
@@ -34,9 +45,10 @@ export default function MainScreen() {
         <br />
         {JoinRoomInputBox(pokemon)}
         <br />
-        <Button><Link style={{ textDecoration: 'none' }} to="../PokemonListScreen/">View Pokemon</Link></Button>
+        <Button><Link style={{ textDecoration: 'none' }} to={`../PokemonListScreen/${player_id}`}>View Pokemon</Link></Button>
         <br />
         <Button><Link style={{ textDecoration: 'none' }} to="../PokemonCaptureScreen/">Capture Pokemon!</Link></Button>
+        <Button onClick={fetchPokemon}>Get a stored pokemon</Button>
 
       </CardContent >
     </Card >
