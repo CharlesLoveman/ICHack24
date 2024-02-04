3f1365a417e0b34d1e38494509de16357c1ddfaa
Eleanor Kneip <eleanor.kneip2705@gmail.com>
2024-02-04 03:35:27 +0000

    Feat: implemented generate_attack.

--- Backend/pokemon.py
+++ Backend/pokemon.py
@@ -135,8 +135,8 @@ class Attack:
 
         if not isinstance(self.power, int):
             raise TypeError(f"Power must be an integer, but {self.power} is a {type(self.power).__name__}")
-        if (not 256 > self.power) or (not self.power > 0):
-            raise ValueError(f"Power must be a positive value less than 256, but {self.power} is not")
+        if (not 385 > self.power) or (not self.power >= 0):
+            raise ValueError(f"Power must be a non-negative value less than 385, but {self.power} is not")
 
         if not isinstance(self.special, bool):
             raise TypeError(f"Special must be a boolean, but {self.special} is a {type(self.special).__name__}")
@@ -148,7 +148,7 @@ class Attack:
                 raise ValueError(f"Status must have valid keys, but {key} is not")
             if not isinstance(self.self_status[key], int):
                 raise TypeError(f"Status effect value must be an integer, but the {key} status {self.self_status[key]} is a {type(self.self_status[key]).__name__}")
-            if (not 128 > self.self_status[key]) or (not self.self_status[key] > 0):
+            if (not 128 > self.self_status[key]) or (not self.self_status[key] > -128):
                 raise ValueError(f"Status effect values must be positive and less than 128, but the {key} status is {self.self_status[key]}")
 
         if not isinstance(self.target_status, dict):
@@ -158,12 +158,104 @@ class Attack:
                 raise ValueError(f"Status must have valid keys, but {key} is not")
             if not isinstance(self.target_status[key], int):
                 raise TypeError(f"Status effect value must be an integer, but the {key} status {self.target_status[key]} is a {type(self.target_status[key]).__name__}")
-            if (not -128 < self.target_status[key]) or (not self.target_status[key] < 0):
+            if (not -128 < self.target_status[key]) or (not self.target_status[key] < 128):
                 raise ValueError(f"Status effect values must be positive and less than 128, but the {key} status is {self.target_status[key]}")
 
     def __repr__(self):
         """Return a string representation of the Attack."""
         return f"Attack({repr(self.name)}, {repr(self.element)}, {repr(self.power)}, {repr(self.special)}, {repr(self.self_status)}, {repr(self.target_status)})"
+
+
+def generate_attack(name : str, element : str, category : str):
+    """Generate a random attack with the given name, element and category."""
+
+    if not isinstance(name, str):
+        raise TypeError(f"Name must be a string, but {name} is a {type(name).__name__}")
+    if not element in element_options:
+        raise ValueError(f"Element must be a valid element, but {element} is not")
+    if not category in ["physical", "special", "status"]:
+        raise ValueError(f"Category must be either 'physical', 'special' or 'status', but {category} is not")
+
+    power = 0
+    special = False
+    self_status = {}
+    target_status = {}
+
+    if category == 'physical':
+        power = np.random.randint(1, 256)
+    elif category == 'special':
+        power = np.random.randint(1, 256)
+        special = True
+        stat_ind = np.random.rand()
+        if stat_ind <= 0.2:
+            good_bad_ind = np.random.rand()
+            if good_bad_ind <= 0.5:
+                # + status to me, - status to them, less powerful
+                stat_changes = min(int(np.random.gamma(1, 1, 1)[0]) + 1, 6)
+                budget = 192
+                while stat_changes > 0 and budget > 0:
+                    me_or_them_ind = np.random.rand()
+                    if me_or_them_ind <= 0.5:
+                        stat_modify_by = int(np.random.gamma(10, 3.5, 100)[0])
+                        budget -= stat_modify_by
+                        if budget < 0:
+                            stat_modify_by += budget
+                        stat_to_modify = np.random.choice(stats_keys)
+                        self_status[stat_to_modify] = stat_modify_by
+                    else:
+                        stat_modify_by = int(np.random.gamma(10, 3.5, 100)[0])
+                        budget -= stat_modify_by
+                        if budget < 0:
+                            stat_modify_by += budget
+                        stat_to_modify = np.random.choice(stats_keys)
+                        target_status[stat_to_modify] = -stat_modify_by
+                    stat_changes -= 1
+                power_change = np.random.randint(50, 100)
+                power = int(power * power_change / 100)
+            else:
+                # - status to me, + status to them, more powerful
+                stat_changes = min(int(np.random.gamma(1, 1, 1)[0]) + 1, 6)
+                budget = 192
+                while stat_changes > 0 and budget > 0:
+                    me_or_them_ind = np.random.rand()
+                    if me_or_them_ind <= 0.5:
+                        stat_modify_by = int(np.random.gamma(10, 3.5, 100)[0])
+                        budget -= stat_modify_by
+                        if budget < 0:
+                            stat_modify_by += budget
+                        stat_to_modify = np.random.choice(stats_keys)
+                        self_status[stat_to_modify] = -stat_modify_by
+                    else:
+                        stat_modify_by = int(np.random.gamma(10, 3.5, 100)[0])
+                        budget -= stat_modify_by
+                        if budget < 0:
+                            stat_modify_by += budget
+                        stat_to_modify = np.random.choice(stats_keys)
+                        target_status[stat_to_modify] = stat_modify_by
+                    stat_changes -= 1
+                power_change = np.random.randint(100, 150)
+                power = int(power * power_change / 100)
+    else:
+        stat_changes = min(int(np.random.gamma(2, 1, 1)[0]) + 1, 6)
+        budget = 256
+        while stat_changes > 0 and budget > 0:
+            me_or_them_ind = np.random.rand()
+            if me_or_them_ind <= 0.5:
+                stat_modify_by = int(np.random.gamma(10, 5, 100)[0])
+                budget -= stat_modify_by
+                if budget < 0:
+                    stat_modify_by += budget
+                stat_to_modify = np.random.choice(stats_keys)
+                self_status[stat_to_modify] = stat_modify_by
+            else:
+                stat_modify_by = int(np.random.gamma(10, 5, 100)[0])
+                budget -= stat_modify_by
+                if budget < 0:
+                    stat_modify_by += budget
+                stat_to_modify = np.random.choice(stats_keys)
+                target_status[stat_to_modify] = stat_modify_by
+            stat_changes -= 1
+    return Attack(name, element, power, special, self_status, target_status)
 
 
 class Battle:
