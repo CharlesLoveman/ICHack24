049c3a973fba4c718942ca8495986123ef8750a0
Akesh Mistry <akeshmistry15@gmail.com>
2024-02-04 07:43:28 +0000

    Modify Dockerfile

--- Frontend/app/src/socket.js
+++ Frontend/app/src/socket.js
@@ -1,10 +1,11 @@
 import { io } from 'socket.io-client';
 
 // "undefined" means the URL will be computed from the `window.location` object
-const URL = process.env.NODE_ENV === 'production' ? undefined : 'http://127.0.0.1:5000';
+const URL = process.env.NODE_ENV === 'production' ? 'http://34.105.220.197:5000' : 'http://127.0.0.1:5000';
 
 export const socket = io(URL, {
-    withCredentials: true,
-    extraHeaders: {
-      "my-custom-header": "abcd"
-    }});
+  withCredentials: true,
+  extraHeaders: {
+    "my-custom-header": "abcd"
+  }
+});
