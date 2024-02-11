8bf99ae0ec4bb0bc25ff4f34b51bbf410d5fda62
Niall Oswald <niall.oswald@hotmail.co.uk>
2024-02-11 13:57:53 +0000

    Fix missing import

--- Backend/gcloud_test/test_mongo.py
+++ Backend/gcloud_test/test_mongo.py
@@ -1,6 +1,7 @@
 """Test MongoDB inside Gcloud."""
 from pymongo import MongoClient
 from dotenv import dotenv_values
+import os
 
 config = dotenv_values(".prod" if os.getenv("FLASK_ENV") == "prod" else ".dev")
 
