125cef6ec3890347dcabd194fdb9a4b7a3d4c4b0
Akesh Mistry <akesh.mistry@caplin.com>
2026-01-25 03:01:02 +0000

    revamp PokemonCaptureScreen

--- Frontend/src/components/layout/Title.tsx
+++ Frontend/src/components/layout/Title.tsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import { HeaderContainer } from "./HeaderContainer";
+import { ReactNode } from "react";
+import { red } from "../../utils/colors";
 
 const TitleContainer = styled.h1`
   margin: 0;
@@ -7,20 +9,20 @@ const TitleContainer = styled.h1`
 `;
 
 const TitleHeaderContainer = styled(HeaderContainer)`
-  background-color: #ff5252;
+  background-color: ${red};
   align-items: center;
   justify-content: center;
   padding: 1rem;
 `;
 
 interface TitleProps {
-  title: string;
+  children: ReactNode;
 }
 
-export const Title: React.FC<TitleProps> = ({ title }) => {
+export const Title: React.FC<TitleProps> = ({ children }) => {
   return (
     <TitleHeaderContainer>
-      <TitleContainer>{title}</TitleContainer>
+      <TitleContainer>{children}</TitleContainer>
     </TitleHeaderContainer>
   );
 };
--- Frontend/src/components/screens/PokemonCaptureScreen.tsx
+++ Frontend/src/components/screens/PokemonCaptureScreen.tsx
@@ -1,10 +1,8 @@
-import { useState, ChangeEvent } from "react";
+import { ChangeEvent } from "react";
 import axios from "axios";
 
 import Input from "@mui/material/Input";
-import Button from "@mui/material/Button";
-import { FaCameraRetro } from "react-icons/fa";
-import { IoIosCheckmarkCircle } from "react-icons/io";
+import { FaCameraRetro } from "react-icons/fa6";
 import { useParams, useNavigate } from "react-router-dom";
 import { GiForest, GiSpikyExplosion } from "react-icons/gi";
 import { FaDog } from "react-icons/fa";
@@ -12,93 +10,110 @@ import { FaBottleWater } from "react-icons/fa6";
 import { POKEMON_HAS_RETURNED } from "../../types";
 import { backendAddress } from "../../env";
 import { useGlobalData } from "../../hooks/useGlobalData";
+import { Title } from "../layout/Title";
+import { Box, Typography } from "@mui/material";
+import styled from "styled-components";
+import { darkGrey, red } from "../../utils/colors";
+
+// On file upload (click the upload button)
+const uploadFile = async function (selectedFile?: File, id?: string) {
+  // Create an object of formData
+
+  const formData = new FormData();
+
+  // Update the formData object
+  if (selectedFile) {
+    formData.append("img", selectedFile);
+    try {
+      const res = await axios.post(
+        `${backendAddress}/CreatePokemon/${id}`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+      return res.data.message;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+};
 
 export default function PokemonCaptureScreen() {
-  const data = useGlobalData();
-  const [state, setState] = useState<{ selectedFile: File } | null>(null);
-  const [url, setURL] = useState<string | undefined>(undefined);
-  const [hidden, setHidden] = useState("");
-
+  const { setPokemonReturned, setNoNewPokemon, noNewPokemon } = useGlobalData();
   const params = useParams();
-
   const navigate = useNavigate();
+
+  const onFileUpload = async function (file?: File) {
+    setPokemonReturned(POKEMON_HAS_RETURNED.WAITING);
+    navigate(-1);
+    await uploadFile(file, params.id);
+    setPokemonReturned(POKEMON_HAS_RETURNED.RETURNED);
+    setNoNewPokemon(noNewPokemon + 1);
+  };
 
   // On file select (from the pop up)
   const onFileChange = (event: ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files.length > 0) {
-      setState({
-        selectedFile: event.target.files[0],
-      });
-
-      setURL(getImage(event.target.files[0]));
-    }
-
-    setHidden("none");
-  };
-
-  // On file upload (click the upload button)
-  const uploadFile = async function () {
-    // Create an object of formData
-    data.setPokemonReturned(POKEMON_HAS_RETURNED.WAITING);
-    const formData = new FormData();
-    navigate(-1);
-
-    // Update the formData object
-    if (state) {
-      if (state.selectedFile) {
-        formData.append("img", state.selectedFile);
-        try {
-          const res = await axios.post(
-            `${backendAddress}/CreatePokemon/${params.id}`,
-            formData,
-            {
-              headers: {
-                "Content-Type": "multipart/form-data",
-              },
-            }
-          );
-          return res.data.message;
-        } catch (err) {
-          console.log(err);
-        }
-      }
+      onFileUpload(event.target.files[0]);
     }
   };
 
-  const onFileUpload = async function () {
-    await uploadFile();
-    data.setPokemonReturned(POKEMON_HAS_RETURNED.RETURNED);
-    data.setNoNewPokemon(data.noNewPokemon + 1);
-  };
+  const CaptureContainer = styled.div`
+    margin: auto;
+    display: grid;
+    place-items: center;
+    grid-template-areas: "inner-div";
+  `;
 
-  function getImage(image: File) {
-    return URL.createObjectURL(image);
-  }
+  const Absolute = styled.div`
+    grid-area: inner-div;
+    position: relative;
+  `;
+
+  const Circle = styled.div`
+    background-color: white;
+    border-radius: 100rem;
+    padding: 0.9rem;
+    height: 2rem;
+    width: 2rem;
+    z-index: 100;
+    position: relative;
+    bottom: -0.9rem;
+  `;
 
   return (
     <>
-      <div style={{ textAlign: "center" }}>
-        <GiForest />
+      <Title>
         Capture a new Pokemon!
         <GiForest />
-      </div>
-      <div style={{ textAlign: "center" }}>
-        <label
-          htmlFor="imageUpload"
-          style={{ cursor: "pointer", textAlign: "center" }}
-        >
-          <FaCameraRetro display={hidden} size="9rem"></FaCameraRetro>
-        </label>
-      </div>
-      <div style={{ textAlign: "center" }}>
-        {url && <img src={url} width="200rem" alt="preview" />}
-      </div>
-      <div style={{ textAlign: "center" }}>
+      </Title>
+      <Typography style={{ textAlign: "center" }} variant="h6">
         Take a picture of an animal <FaDog />, object <FaBottleWater /> or
-        anything else <GiSpikyExplosion /> you would like to Pokefy and upload
-        it here. If you're happy with the result, click the tick to adopt the
-        Pokemon. Happy capturing!
-      </div>
+        anything else <GiSpikyExplosion /> you would like to Pokefy!
+      </Typography>
+      <CaptureContainer>
+        <Absolute>
+          <Circle></Circle>
+        </Absolute>
+        <Absolute>
+          <label htmlFor="imageUpload" style={{ cursor: "pointer" }}>
+            <Box
+              style={{
+                color: darkGrey,
+                borderRadius: "100rem",
+                backgroundColor: red,
+                padding: "5rem",
+                zIndex: "0",
+              }}
+            >
+              <FaCameraRetro size="10rem"></FaCameraRetro>
+            </Box>
+          </label>
+        </Absolute>
+      </CaptureContainer>
 
       <Input
         id="imageUpload"
@@ -107,11 +122,6 @@ export default function PokemonCaptureScreen() {
         style={{ display: "none" }}
         inputProps={{ accept: "image/*;capture=camera" }}
       />
-
-      <Button fullWidth={true} onClick={onFileUpload}>
-        {" "}
-        <IoIosCheckmarkCircle size="10rem" />
-      </Button>
     </>
   );
 }
